018c723ad929ac3ed7776c3e7c0e76b405ad70f0
kenellorando <kenellorando@gmail.com>
2017-03-01 23:57:16 -0600

    Changed spacing to 2

--- js/player.js
+++ js/player.js
@@ -1,16 +1,16 @@
 function playerToggle() {
-	var stream = document.getElementById("stream");
+  var stream = document.getElementById("stream");
 
-	if (stream.paused) {
-		stream.play();
-		document.getElementById("playerToggle").innerHTML = "❚❚";
-	} else {
-		location.reload(); // The mindblowing workaround to a desyncing pause button
-		document.getElementById("playerToggle").innerHTML = "►";
-	}
+  if (stream.paused) {
+    stream.play();
+    document.getElementById("playerToggle").innerHTML = "❚❚";
+  } else {
+    location.reload(); // The mindblowing workaround to a desyncing pause button
+    document.getElementById("playerToggle").innerHTML = "►";
+  }
 }
 
 function volumeToggle(vol) {
-	var volume = document.getElementById("stream");
-	volume.volume = vol / 100;
+  var volume = document.getElementById("stream");
+  volume.volume = vol / 100;
 }

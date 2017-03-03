e1cbc21959944cca7fef21671911be090a1cd799
Ken Ellorando <Ken Ellorando>
2017-03-03 04:53:32 -0600

    Changed theme. Added new media

--- js/themeChanger.js
+++ js/themeChanger.js
@@ -1,6 +1,6 @@
 function selectChicagoEvening() {
   document.getElementById("selectedcss").href = "/css/themes/chicagoEvening.css";
-  document.getElementById("subtitle").innerHTML = "A Study/Relax Chillhop Radio";
+  document.getElementById("subtitle").innerHTML = "A Rhythmic Experience";
 }
 
 function selectCyberpunkBartender() {

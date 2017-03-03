dba5ac99bf62a4b081b6ee1d496d70f915d83273
Ken Ellorando <Ken Ellorando>
2017-03-03 05:08:26 -0600

    ignore
    Former-commit-id: 7fdb1ed0944439f89ed28a8f5e135eb3ae7f78a1

--- js/themeChanger.js
+++ js/themeChanger.js
@@ -3,6 +3,7 @@ function selectChicagoEvening() {
   document.getElementById("subtitle").innerHTML = "A Rhythmic Experience";
 }
 
+
 function selectCyberpunkBartender() {
   document.getElementById("selectedcss").href = "/css/themes/cyberpunkBartender.css";
   document.getElementById("subtitle").innerHTML = "A Retro Cyberpunk Jukebox";

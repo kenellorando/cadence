c5d500fca898b2975f4c2494931eeb82bc4d8eaa
Ryan Hodin <ryan951357@gmail.com>
2017-03-04 21:53:53 -0600

    Update theme tracker when themes are updated
    Using the themeID helper from player.js, and searching by name (so that
    themeIDs can be reordered later to no ill effect)

--- js/theme-changer.js
+++ js/theme-changer.js
@@ -1,10 +1,12 @@
 function selectChicagoEvening() {
   document.getElementById("selected-css").href = "/css/themes/chicago-evening.css";
   document.getElementById("subtitle").innerHTML = "A Rhythmic Experience";
+  document.getElementById("theme-tracker").value = themeID("chicago-evening");
 }
 
 
 function selectCyberpunkBartender() {
   document.getElementById("selected-css").href = "/css/themes/cyberpunk-bartender.css";
   document.getElementById("subtitle").innerHTML = "A Retro Cyberpunk Jukebox";
+  document.getElementById("theme-tracker").value = themeID("cyberpunk-bartender");
 }

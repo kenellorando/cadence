f980be3d99a96455a5e14bfee3e190729dad48e4
Ken Ellorando <Ken Ellorando>
2017-03-22 23:57:17 -0500

    Beautified

--- database/db_command_list.sql
+++ database/db_command_list.sql
@@ -1,6 +1,6 @@
 /* List of example commands for a music database
-	supports song titles, artist names, and file paths
-    Tested for LASTORDER's database*/
+supports song titles, artist names, and file paths
+Tested for LASTORDER's database*/
     
 /* DB CREATE */
 SHOW databases;
@@ -12,20 +12,17 @@ DROP TABLE songs;
 
 /* TABLE CREATION */
 CREATE TABLE songs (
-	song_id INTEGER NOT NULL AUTO_INCREMENT,
-	song_title VARCHAR(100) NOT NULL,
+    song_id INTEGER NOT NULL AUTO_INCREMENT,
+    song_title VARCHAR(100) NOT NULL,
     artist_name VARCHAR(50) DEFAULT NULL,
     song_path VARCHAR(100) NOT NULL,
-	
-    
-	PRIMARY KEY (song_id)
+    PRIMARY KEY (song_id)
 );
 
 /* DESCRIBE TABLES */
 EXPLAIN songs;
 
-/* INSERTS 
-	note the double backslash*/
+/* INSERTS, note the double backslash*/
 INSERT INTO songs (song_title, artist_name, song_path)
 VALUES ("database", "MAN WITH A MISSION", "C:\\Users\\kenel\\Music\\MAN WITH A MISSION\\01 - database feat.TAKUMA (10-FEET).mp3");
 
@@ -34,6 +31,21 @@ VALUES ("Hello,world!", "BUMP OF CHICKEN", "C:\\Users\\kenel\\Music\\BUMP OF CHI
 
 
 /* SELECTS */
-SELECT * FROM songs;
-SELECT * FROM songs WHERE artist_name = "BUMP OF CHICKEN";
-SELECT * FROM songs WHERE song_title = "database";
+SELECT 
+    *
+FROM
+    songs;
+    
+SELECT 
+    *
+FROM
+    songs
+WHERE
+    artist_name = 'BUMP OF CHICKEN';
+    
+SELECT 
+    *
+FROM
+    songs
+WHERE
+    song_title = 'database';

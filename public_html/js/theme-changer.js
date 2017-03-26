52794d072de38000fd41b911b1b65a5a30b20f89
Ken Ellorando <Ken Ellorando>
2017-03-26 01:08:47 -0500

    Modified the cyberpunk bartender theme

--- public_html/js/theme-changer.js
+++ public_html/js/theme-changer.js
@@ -6,5 +6,6 @@ function selectChicagoEvening() {
 
 function selectCyberpunkBartender() {
   document.getElementById("selected-css").href = "/css/themes/cyberpunk-bartender.css";
+  document.getElementById("title").innerHTML = "CADEN<span>C</span>E";
   document.getElementById("subtitle").innerHTML = "A Retro Cyberpunk Jukebox";
 }

3571148e123f90389e3a3c275402580374166862
Ken Ellorando <Ken Ellorando>
2017-03-26 03:26:59 -0500

    Better to just leave the spans in the plain (for now at least)
    Former-commit-id: b1ba26b0806a5e0e84d7a0fc74b6c64369391119

--- public_html/js/theme-changer.js
+++ public_html/js/theme-changer.js
@@ -3,9 +3,7 @@ function selectChicagoEvening() {
   document.getElementById("subtitle").innerHTML = "A Rhythmic Experience";
 }
 
-
 function selectCyberpunkBartender() {
   document.getElementById("selected-css").href = "/css/themes/cyberpunk-bartender.css";
-  document.getElementById("title").innerHTML = "CADEN<span>C</span>E";
   document.getElementById("subtitle").innerHTML = "A Retro Cyberpunk Jukebox";
 }

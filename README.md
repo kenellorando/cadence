8f6edd9bc07d30c4e35838f41ff3cac4258b0d14
Ken Ellorando <Ken Ellorando>
2017-03-27 02:37:13 -0500

    Playing with branches

---
+++ README.md
@@ -0,0 +1,13 @@
+# Cadence Radio (http://www.kenellorando.com)
+## Background
+Cadence is an online radio heavily inspired by 4chan/8ch's [R/a/dio](http://r-a-d.io/). The project is my first endeavour to practice a full-range of IT-skills. It currently utilizes technologies in front-end design with HTML and CSS, site scripting with JavaScript, and some networking and server management using Windows Server 2016. Back-end work for a music database is also currently being built using the MySQL RDBMS. The database is filled with thousands of non-redundant entries of music metadata using a populator tool I programmed in Java.
+
+Future plans for the radio include a front-end database query feature (PHP) and an advanced back-end request bot (C#).
+
+## Acknowledgements
+### Technical Assistance
+* [Ryan Hodin](https://github.com/za419) (HTML, CSS, JS)
+* [Bobby Ton](https://github.com/bobbyt1997) (Java)
+### User Testers
+* Michael Folk
+* Jakob Frank
---
+++ public_html/js/theme-changer.js
@@ -0,0 +1,24 @@
+
+function selectChicagoEvening() {
+  document.getElementById("selected-css").href = "/css/themes/chicago-evening.css";
+  document.getElementById("subtitle").innerHTML = "A Rhythmic Experience";
+  localStorage.setItem('themeKey', 'chicagoEvening');
+}
+
+function selectCyberpunkBartender() {
+  document.getElementById("selected-css").href = "/css/themes/cyberpunk-bartender.css";
+  document.getElementById("subtitle").innerHTML = "A Retro Cyberpunk Jukebox";
+  localStorage.setItem('themeKey', 'cyberpunkBartender');
+}
+
+// This is run onload. To change the default theme, (for users that have not yet picked one) change the statement for null
+function defaultTheme() {
+  var theme = localStorage.getItem('themeKey');
+  if (theme === "chicagoEvening") {
+    selectChicagoEvening();
+  } else if (theme === "cyberpunkBartender") {
+    selectCyberpunkBartender();
+  } else if (theme === null) {
+    selectChicagoEvening();
+  }
+}

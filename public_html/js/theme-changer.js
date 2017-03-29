5f90153d07feb4c38ffae0d18d47cd9d1dd42bba
Ryan Hodin <ryan951357@gmail.com>
2017-03-29 01:34:09 -0500

    Force video reload when ISS theme is selected - Fixes #24
    The last commit caused the browser to no longer load the video, perhaps
    because it had 'learned' that the video did not exist. We now reload it
    every time the Space Station theme is selected.
    
    This does, however, mean that selecting Space Station causes the video to
    restart. I don't want that, so I'll be looking into a fix before I push
    this.

--- public_html/js/theme-changer.js
+++ public_html/js/theme-changer.js
@@ -18,6 +18,7 @@ function selectSpaceStation() {
   // Not sure how to get this to work. Want to keep the src empty until this is activated. Then remove it when another is selected
   var video = document.getElementById("video-source");
   video.src = "/media/iss.mp4";
+  video.parentElement.load();
 
 }
 

2a23050f18791e017d0d1554e79c658287dd9492
Ryan Hodin <ryan951357@gmail.com>
2017-03-30 02:39:25 -0500

    Make cyberpunkBackgroundSwitcher randomize cyberpunk background
    This is currently unweighted, and never even loaded into the page.

--- public_html/js/cyberpunk-bartender.js
+++ public_html/js/cyberpunk-bartender.js
@@ -1,4 +1,16 @@
 // Handles the switching of the cyberpunk bartender background
 function cyberpunkBackgroundSwitcher() {
+    var URLs = [
+    "cyberpunk-bartender.gif",
+    "cyberpunk-bartender2.gif",
+    "cyberpunk-bartender3.gif"];
+    var URLroot = document.location.href;
+    URLroot = URLroot.substring(0, URLroot.lastIndexOf("/")+1);
+    URLroot += "media/";
+
+    var index = Math.floor(Math.random() * URLs.length);
     
+    var url = URLroot+URLs[index]
+
+    document.getElementsByTagName("html")[0].style.backgroundImage = "url("+url+")";
 }

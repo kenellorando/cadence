69db31930a8eb550bc629abe1495b34ba9c1d3d8
kenellorando <kenellorando@gmail.com>
2017-03-29 22:20:41 -0500

    Seeing what happens if I don't build the url
    Former-commit-id: c77caad41bea41807853aa510bfb44a703f65f91

--- public_html/js/theme-changer.js
+++ public_html/js/theme-changer.js
@@ -14,11 +14,14 @@ function selectSpaceStation() {
   document.getElementById("selected-css").href = "/css/themes/space-station.css";
   document.getElementById("subtitle").innerHTML = "A Space Odyssey";
   localStorage.setItem('themeKey', 'iss');
- 
-  // Not sure how to get this to work. Want to keep the src empty until this is activated. Then remove it when another is selected
-  var video = document.getElementById("video-source");
-  video.src = "/media/iss.mp4";
 
+  var video = document.getElementById("video-source");
+
+  // Loads the video source
+  if (video.src != "http://kenellorando.com/media/iss.mp4") {
+    video.src = "http://kenellorando.com/media/iss.mp4";
+    video.parentElement.load(); // The parent element of video is the div "fullscreen-bg"
+  }
 }
 
 // This is run onload. To change the default theme, (for users that have not yet picked one) change the statement for null

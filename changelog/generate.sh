5b8325e1787b5613962ae93d1d6f8a788013b991
Ryan Hodin <ryan951357@gmail.com>
2017-04-15 04:32:51 -0500

    Add header and footer files
    Which will be, respectively, prepended and appended to the output of the
    changelog generator. Thus, these let one style the output of the
    generator.

--- changelog/generate.sh
+++ changelog/generate.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-generator/changelog-generator.sh -t .. -o ../public_html/changelog.html
+generator/changelog-generator.sh -t .. -o content
+
+cat ./header ./content ./footer > ../public_html/changelog.html
+
+rm -f ./content

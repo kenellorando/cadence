99e9a8f719afcd5279c84957dcd76fe688754adc
kenellorando <kenellorando@gmail.com>
2017-04-30 00:41:41 -0500

    Created electromaster theme
    Former-commit-id: 42ff66a76c8901e1b1de78a8a1e37b484f942d43

--- public_html/js/theme-changer.js
+++ public_html/js/theme-changer.js
@@ -1,22 +1,29 @@
 function selectChicagoEvening() {
- 
   document.getElementById("selected-css").href = "/css/themes/chicago-evening.css";
+  document.getElementById("title").innerHTML = "CADENCE";
   document.getElementById("subtitle").innerHTML = "A Rhythmic Experience";
   localStorage.setItem('themeKey', 'chicagoEvening');
 }
 
 function selectCyberpunkBartender() {
-  
   document.getElementById("selected-css").href = "/css/themes/cyberpunk-bartender.css";
+  document.getElementById("title").innerHTML = "CADEN<span>C</span>E";
   document.getElementById("subtitle").innerHTML = "A Retro Cyberpunk Jukebox";
   localStorage.setItem('themeKey', 'cyberpunkBartender');
 }
 
 function selectMayberry() {
-  
   document.getElementById("selected-css").href = "/css/themes/mayberry.css";
+  document.getElementById("title").innerHTML = "CADENCE";
   document.getElementById("subtitle").innerHTML = "A Rhythmic Ξxperience";
   localStorage.setItem('themeKey', 'mayberry');
+}
+
+function selectElectromaster() {
+  document.getElementById("selected-css").href = "/css/themes/electromaster.css";
+  document.getElementById("title").innerHTML = "ケイデンス";
+  document.getElementById("subtitle").innerHTML = "A Certain Scientific Radio";
+  localStorage.setItem('themeKey', 'electromaster');
 }
 
 // This is run onload. To change the default theme, (for users that have not yet picked one) change the statement for null
@@ -28,6 +35,8 @@ function defaultTheme() {
     selectCyberpunkBartender();
   } else if (theme === "mayberry") {
     selectMayberry();
+  } else if (theme === "electromaster") {
+    selectElectromaster();
   } else if (theme === null) {
     selectChicagoEvening();
   }

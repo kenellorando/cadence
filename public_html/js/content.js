34ac158a0f4fabe79f163b146aa16d7f993ba2c8
kenellorando <kenellorando@gmail.com>
2017-05-15 01:32:25 -0500

    Moved scripts to new content js file

---
+++ public_html/js/content.js
@@ -0,0 +1,17 @@
+jQuery(function ($) {
+  $('#statusMusicStream').load('/status/index.php #statusMusicStream');
+  $('#statusSongDatabase').load('/status/index.php #statusSongDatabase');
+  $('#statusWebserverFTP').load('/status/index.php #statusWebserverFTP');
+});
+
+function show() {
+  var old = document.getElementById("old");
+
+  if (old.style.display === 'block') {
+    old.style.display = 'none';
+    document.getElementById("oldToggle").innerHTML = "Show Full History";
+  } else {
+    old.style.display = 'block';
+    document.getElementById("oldToggle").innerHTML = "Hide Full History";
+  }
+}

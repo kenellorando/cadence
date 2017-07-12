fce6f927a3c68219234484574da0c7e91f26a02b
kenellorando <kenellorando@gmail.com>
2017-07-12 03:13:17 -0500

    Consolidated theme functions into one master theme changer
    Former-commit-id: 1c545a59d75b70f930888c8237392424f6b154b0

--- public_html/js/theme-changer.js
+++ public_html/js/theme-changer.js
@@ -4,114 +4,124 @@ function setThemeColor(color) {
   document.getElementById("ie-color").content = color;
 }
 
-function selectChicagoEvening() {
-  document.getElementById("selected-css").href = "/css/themes/chicago-evening.css";
-  document.getElementById("title").innerHTML = "CADENCE";
-  document.getElementById("subtitle").innerHTML = "A Rhythmic Experience";
+// 
+function themeChanger(themeName) {
+  var changeTo = {};
 
-  setThemeColor("#1D2951"); // A dark navy
-
-  localStorage.setItem('themeKey', 'chicagoEvening');
-}
-
-function selectCyberpunkBartender() {
-  document.getElementById("title").innerHTML = "CADENCE";
-  document.getElementById("subtitle").innerHTML = "A Retro Cyberpunk Jukebox";
-
-  var currentHour = new Date().getHours();
-
-  // IF condition states the daytime hours
-  // 8:00:00 PM - 9:59:59 AM
-  if (currentHour >= 8 && currentHour < 22) {
-    document.getElementById("selected-css").href = "/css/themes/cyberpunk-bartender.css";
-    setThemeColor("#B30E67"); // Deeppink, with Value (HSV) set to 70 (from 100)
-  } else {
-    document.getElementById("selected-css").href = "/css/themes/cyberpunk-bartender-night.css";
-    setThemeColor("#1D2951"); // A dark navy
+  // CHICAGO EVENING ******************
+  if (themeName == "chicagoEvening") {
+    changeTo = {
+      css: "/css/themes/chicago-evening.css",
+      title: "CADENCE",
+      subtitle: "A Rhythmic Experience",
+      themeColor: "#1d2951", // Navy
+      themeKey: "chicagoEvening"
+    };
   }
 
-  localStorage.setItem('themeKey', 'cyberpunkBartender');
-}
-
-function selectMayberry() {
-  document.getElementById("selected-css").href = "/css/themes/mayberry.css";
-  document.getElementById("title").innerHTML = "CADENCE";
-  document.getElementById("subtitle").innerHTML = "A Rhythmic Ξxperience";
-
-  setThemeColor("#000000"); // Black
-
-  localStorage.setItem('themeKey', 'mayberry');
-}
-
-function selectElectromaster() {
-  document.getElementById("selected-css").href = "/css/themes/electromaster.css";
-  document.getElementById("title").innerHTML = "ケイデンス";
-  document.getElementById("subtitle").innerHTML = "A Certain Scientific Radio";
-
-  setThemeColor("#09C1FF"); // A certain scientific light blue
-
-  localStorage.setItem('themeKey', 'electromaster');
-}
-
-function selectStarGuardian() {
-  document.getElementById("selected-css").href = "/css/themes/star-guardian.css";
-  document.getElementById("title").innerHTML = "<span id='ke'>ケ</span><span id='i'>イ</span><span id='de'>デ</span><span id ='n'>ン</span><span id='su'>ス</span>";
-  document.getElementById("subtitle").innerHTML = "A Stellar Experience";
-
-  setThemeColor("#ecb1e9"); // Lux pink
-
-  localStorage.setItem('themeKey', 'starGuardian');
-}
-
-function selectLightMage() {
-  document.getElementById("selected-css").href = "/css/themes/light-mage.css";
-  document.getElementById("title").innerHTML = "CADENCE";
-  document.getElementById("subtitle").innerHTML = "Just An Ordinary Radio";
-  localStorage.setItem('themeKey', 'lightMage');
-
-  setThemeColor("#FFFFFF"); // white
-
-  var video = document.getElementById("video-source");
-
-  // Quick and dirty fix to get absolute URL to fix the stuttering background
-  var filename = document.location + "/media/lux1.mp4";
-
-  // Loads the video source
-  if (video.src != filename) {
-    video.src = filename;
-    video.parentElement.load(); // The parent element of video is the div "fullscreen-bg"
+  // CYBERPUNK *************************
+  else if (themeName == "cyberpunkBartender") {
+    var currentHour = new Date().getHours();
+    // IF condition states the daytime hours
+    // 8:00:00 PM - 9:59:59 AM
+    if (currentHour >= 8 && currentHour < 22) {
+      changeTo = {
+        css: "/css/themes/cyberpunk-bartender.css",
+        title: "CADENCE",
+        subtitle: "A Retro Cyberpunk Jukebox",
+        themeColor: "#B30E67", // Hot pink
+        themeKey: "cyberpunkBartender"
+      };
+    } else {
+      changeTo = {
+        css: "/css/themes/cyberpunk-bartender-night.css",
+        title: "CADENCE",
+        subtitle: "A Retro Cyberpunk Jukebox",
+        themeColor: "#1D2951", // Navy
+        themeKey: "cyberpunkBartender"
+      };
+    }
   }
-}
 
-function selectYorha() {
-  document.getElementById("selected-css").href = "/css/themes/yorha.css";
-  document.getElementById("title").innerHTML = "CADENCE";
-  document.getElementById("subtitle").innerHTML = "For The Glory of Mankind";
+  // MAYBERRY ***************************
+  else if (themeName == "mayberry") {
+    changeTo = {
+      css: "/css/themes/mayberry.css",
+      title: "CADENCE",
+      subtitle: "A Rhythmic Ξxperience",
+      themeColor: "#000000", // Black
+      themeKey: "mayberry"
+    };
+  }
 
-  setThemeColor("#000000"); // Black
+  // ELECTROMASTER **********************
+  else if (themeName == "electromaster") {
+    changeTo = {
+      css: "/css/themes/electromaster.css",
+      title: "ケイデンス",
+      subtitle: "A Certain Scientific Radio",
+      themeColor: "#09C1FF", // A certain scientific light blue
+      themeKey: "electromaster"
+    };
+  }
 
-  localStorage.setItem('themeKey', 'yorha');
+  // STAR GUARDIAN *******************
+  else if (themeName == "starGuardian") {
+    changeTo = {
+      css: "/css/themes/star-guardian.css",
+      title: "<span id='ke'>ケ</span><span id='i'>イ</span><span id='de'>デ</span><span id ='n'>ン</span><span id='su'>ス</span>",
+      subtitle: "A Stellar Experience",
+      themeColor: "#ecb1e9", // Lux pink
+      themeKey: "starGuardian"
+    };
+  }
+
+  // LIGHT MAGE ***********************
+  else if (themeName == "lightMage") {
+    changeTo = {
+      css: "/css/themes/light-mage.css",
+      title: "CADENCE",
+      subtitle: "Just An Ordinary Radio",
+      themeColor: "#FFFFFF", // White
+      themeKey: "lightMage"
+    };
+
+    var video = document.getElementById("video-source");
+    // Quick and dirty fix to get absolute URL to fix the stuttering background
+    var filename = document.location + "/media/lux1.mp4";
+    // Loads the video source
+    if (video.src != filename) {
+      video.src = filename;
+      video.parentElement.load(); // The parent element of video is the div "fullscreen-bg"
+    }
+  }
+
+  // YORHA ******************************
+  else if (themeName == "yorha") {
+    changeTo = {
+      css: "/css/themes/yorha.css",
+      title: "CADENCE",
+      subtitle: "For The Glory of Mankind",
+      themeColor: "#000000", // Black
+      themeKey: "yorha"
+    };
+  }
+
+  // Change HTML to changeTo's properties
+  document.getElementById("selected-css").href = changeTo.css;
+  document.getElementById("title").innerHTML = changeTo.title;
+  document.getElementById("subtitle").innerHTML = changeTo.subtitle;
+  setThemeColor(changeTo.themeColor); // A dark navy
+  localStorage.setItem('themeKey', changeTo.themeKey);
 }
 
 // This is run onload. To change the default theme, (for users that have not yet picked one) change the statement for null
 function defaultTheme() {
   var theme = localStorage.getItem('themeKey');
-  if (theme === "chicagoEvening") {
-    selectChicagoEvening();
-  } else if (theme === "cyberpunkBartender") {
-    selectCyberpunkBartender();
-  } else if (theme === "mayberry") {
-    selectMayberry();
-  } else if (theme === "electromaster") {
-    selectElectromaster();
-  } else if (theme === "starGuardian") {
-    selectStarGuardian();
-  } else if (theme === "lightMage") {
-    selectLightMage();
-  } else if (theme === "yorha") {
-    selectYorha();
-  } else if (theme === null) {
-    selectChicagoEvening();
+  if (theme == null) {
+    themeChanger("chicagoEvening");
+  } else {
+    themeChanger(theme);
   }
 }
 

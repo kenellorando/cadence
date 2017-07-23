4cc137b700fe551f3705b7abe3eaefd51d1313bc
kenellorando <kenellorando@gmail.com>
2017-07-22 22:51:49 -0500

    Moved themes without special functions to JSON

--- public_html/js/theme-changer.js
+++ public_html/js/theme-changer.js
@@ -1,10 +1,10 @@
-"use strict";
 // Helper function: Does the work of setting theme color for all meta tags
 function setThemeColor(color) {
   document.getElementById("chrome-color").content = color;
   document.getElementById("ie-color").content = color;
 }
 
+// Handles clicks 
 $(document).ready(function () {
   $('.themeChoice').on('click', function () {
     var themeChoice = $(this).attr('id');
@@ -12,23 +12,20 @@ $(document).ready(function () {
   });
 });
 
-
 function themeChanger(themeName) {
-  var changeTo = {};
+  // Returns the specific theme as a single object
+  var themeObj = theme[themeName];
+  console.log(themeObj);
 
-  // CHICAGO EVENING ******************
-  if (themeName === "chicagoEvening") {
-    changeTo = {
-      css: "/css/themes/chicago-evening.css",
-      title: "CADENCE",
-      subtitle: "A Rhythmic Experience",
-      themeColor: "#1d2951", // Navy
-      themeKey: "chicagoEvening"
-    };
-  }
+  document.getElementById("selected-css").href = themeObj.cssPath;
+  document.getElementById("title").innerHTML = themeObj.title;
+  document.getElementById("subtitle").innerHTML = themeObj.subtitle;
+  setThemeColor(themeObj.themeColor);
+  localStorage.setItem('themeKey', themeObj.themeKey);
+
 
   // CYBERPUNK *************************
-  else if (themeName === "cyberpunkBartender") {
+  if (themeName === "cyberpunkBartender") {
     var currentHour = new Date().getHours();
     // IF condition states the daytime hours
     // 8:00:00 PM - 9:59:59 AM
@@ -51,39 +48,6 @@ function themeChanger(themeName) {
     }
   }
 
-  // MAYBERRY ***************************
-  else if (themeName === "mayberry") {
-    changeTo = {
-      css: "/css/themes/mayberry.css",
-      title: "CADENCE",
-      subtitle: "A Rhythmic Ξxperience",
-      themeColor: "#000000", // Black
-      themeKey: "mayberry"
-    };
-  }
-
-  // ELECTROMASTER **********************
-  else if (themeName === "electromaster") {
-    changeTo = {
-      css: "/css/themes/electromaster.css",
-      title: "ケイデンス",
-      subtitle: "A Certain Scientific Radio",
-      themeColor: "#09C1FF", // A certain scientific light blue
-      themeKey: "electromaster"
-    };
-  }
-
-  // STAR GUARDIAN *******************
-  else if (themeName === "starGuardian") {
-    changeTo = {
-      css: "/css/themes/star-guardian.css",
-      title: "<span id='ke'>ケ</span><span id='i'>イ</span><span id='de'>デ</span><span id ='n'>ン</span><span id='su'>ス</span>",
-      subtitle: "A Stellar Experience",
-      themeColor: "#ecb1e9", // Lux pink
-      themeKey: "starGuardian"
-    };
-  }
-
   // LIGHT MAGE ***********************
   else if (themeName === "lightMage") {
     changeTo = {
@@ -103,24 +67,6 @@ function themeChanger(themeName) {
       video.parentElement.load(); // The parent element of video is the div "fullscreen-bg"
     }
   }
-
-  // YORHA ******************************
-  else if (themeName === "yorha") {
-    changeTo = {
-      css: "/css/themes/yorha.css",
-      title: "CADENCE",
-      subtitle: "For The Glory of Mankind",
-      themeColor: "#000000", // Black
-      themeKey: "yorha"
-    };
-  }
-
-  // Change HTML to changeTo's properties
-  document.getElementById("selected-css").href = changeTo.css;
-  document.getElementById("title").innerHTML = changeTo.title;
-  document.getElementById("subtitle").innerHTML = changeTo.subtitle;
-  setThemeColor(changeTo.themeColor); // A dark navy
-  localStorage.setItem('themeKey', changeTo.themeKey);
 }
 
 // This is run onload. To change the default theme, (for users that have not yet picked one) change the statement for null
@@ -136,4 +82,4 @@ function defaultTheme() {
 // Reselects for time-based themes at a set interval
 window.setInterval(function () {
   defaultTheme();
-}, 1000);
+}, 10000);

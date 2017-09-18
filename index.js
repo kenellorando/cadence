bba04b203dc4d1e1be85e6a943d93a47f7e2d68c
kenellorando <kenellorando@gmail.com>
2017-09-17 22:09:29 -0500

    Added a form body parser

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const PORT = 8080;
 const IP = '198.37.25.185';
 
-var express = require('express'),
-app = express(),
-path = require('path');
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var app = express();
+
+
+var urlencodedParser = bodyParser.urlencoded({
+    extended: false
+});
+
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.post('/search', urlencodedParser, function (req, res) {
+    console.log(req.body);
+});
+
+
+
 
 var server = app.listen(PORT, IP);

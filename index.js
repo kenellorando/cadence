40bb5b4a7d2c1df960f66913fee0edd3c2c23a43
kenellorando <kenellorando@gmail.com>
2017-09-19 02:06:30 -0500

    Pass input with AJAX without page reload to ARIA

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 const PORT = 8080;
-const IP = '198.37.25.185';
+const IP = 'localhost';
 
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
 
-var urlencodedParser = bodyParser.urlencoded({
-    extended: false
-});
-
-
+// Point to publicly served files
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.post('/search', urlencodedParser, function (req, res) {
-    console.log(req.body);
-});
+app.post('/search', function (req, res) {
+    // Only visible on the web server console
+    var reqBody = JSON.stringify(req.body);
+    console.log("Received: " + reqBody);
 
+    // Pass response back
+    res.send("OK RESPONSE FROM ARIA BACKEND");
+    res.end();
+});
 
 
 

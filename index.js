c05520bb66476d95bcd9845beb395a3b5036c382
kenellorando <kenellorando@gmail.com>
2017-09-19 03:00:37 -0500

    Beautify

--- index.js
+++ index.js
@@ -8,21 +8,24 @@ var app = express();
 
 
 // Parse incoming data
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
 // Point to publicly served files
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 // Search, directed from aria.js AJAX
 app.post('/search', function (req, res) {
-    console.log("Received: " + JSON.stringify(req.body));
-    // Web Server Console:
-    // Received: {"search":"railgun"}
+  console.log("Received: " + JSON.stringify(req.body));
+  // Web Server Console:
+  // Received: {"search":"railgun"}
 
-    // TODO: Use req data here
+  // TODO: Use req data here
 
-    res.send("OK from ARIA!");
-    res.end();
+  res.send("OK from ARIA!");
+  res.end();
 });
 
 var server = app.listen(PORT, IP);
--- public/js/aria.js
+++ public/js/aria.js
@@ -1,18 +1,18 @@
 /**
  * ARIA's Frontend Functionality
  */
-$(document).ready(function() {
-    $('#searchButton').click(function(e) {
+$(document).ready(function () {
+  $('#searchButton').click(function (e) {
 
-        // Create a key 'search' to send in JSON
-        var data = {};
-        data.search = $('#searchInput').val();
+    // Create a key 'search' to send in JSON
+    var data = {};
+    data.search = $('#searchInput').val();
 
-        $.ajax({
-            type: 'POST',
-            url: 'http://localhost:8080/search',
-            dataType: 'application/json',
-            data: data,
-        });
+    $.ajax({
+      type: 'POST',
+      url: 'http://localhost:8080/search',
+      dataType: 'application/json',
+      data: data,
     });
+  });
 })

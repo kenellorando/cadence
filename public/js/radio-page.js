f085ce09222fafa41579ed690e5999fbc29b13b8
kenellorando <kenellorando@gmail.com>
2017-09-20 01:33:40 -0500

    Revert to f372020145692378d32a8fb431dc7465704491b1 [formerly 8c03992576e9afe6602d035effa242231bc10a59]
    Former-commit-id: b5a528710d31ff4157563fc3d84280015f25a118

--- public/js/radio-page.js
+++ public/js/radio-page.js
@@ -2,6 +2,7 @@
 Organization of radio-page JS
 -----------------------------
 -Retrieve set volume from localstorage
+-Retrieve service status from the status subpage
 
 -Radio now playing artist/track info
   -Update info at an interval function
@@ -12,12 +13,20 @@ Organization of radio-page JS
 -Changelog show/hide button
 */
 
-$(document).ready(function() {
+// I'll put all the default onload stuff in here
+function defaultPlayer() {
   // Selects either the localstorage volume or a default value
   var vol = localStorage.getItem('volumeKey') || 0.77;
   document.getElementById("volume").value = vol;
   var volume = document.getElementById("stream");
   volume.volume = vol;
+}
+
+// Loads up the statuses from the /status/index.php page onto the radio page automatically
+jQuery(function ($) {
+  $('#statusMusicStream').load('/status/index.php #statusMusicStream');
+  $('#statusSongDatabase').load('/status/index.php #statusSongDatabase');
+  $('#statusWebserverFTP').load('/status/index.php #statusWebserverFTP');
 });
 
 // GETS and displays currently playing info

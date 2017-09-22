f447b69d0b19c4807c2f3986029aae2c70d96d00
kenellorando <kenellorando@gmail.com>
2017-09-22 15:36:12 -0500

    Move and rename now playing GET function
    Former-commit-id: e59b80c9b45fba24a7253b4cfcbd4f9a337ac534

--- public/js/radio-page.js
+++ public/js/radio-page.js
@@ -1,5 +1,54 @@
-// GETS and displays currently playing info
-function radioTitle() {
+$(document).ready(function () {
+  // Get the localstorage volume or set to 77%
+  setVolume();
+
+  // Play/pause button
+  document.getElementById("playerToggle").addEventListener('click', function(){
+    streamToggle();
+  });
+
+  // Refresh radio title at an interval
+  setTimeout(function () {
+    nowPlaying();
+  }, 0);
+  setInterval(function () {
+    nowPlaying();
+  }, 10000);
+});
+
+
+// Selects either the localstorage volume or a default value
+function setVolume() {
+  var vol = localStorage.getItem('volumeKey') || 0.77;
+  document.getElementById("volume").value = vol;
+  var volume = document.getElementById("stream");
+  volume.volume = vol;
+}
+
+// The play/pause button
+function streamToggle() {
+  var stream = document.getElementById("stream");
+  var mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent);
+  if (stream.paused) {
+    // Loads up the real stream again if mobile
+    if (mobile) {
+      stream.src = "http://cadenceradio.com:8000/cadence1";
+    }
+    stream.load();
+    stream.play();
+    document.getElementById("playerToggle").innerHTML = "❚❚";
+  } else {
+    // Loads up nothing if mobile
+    if (mobile) {
+      stream.src = "";
+    }
+    stream.load();
+    document.getElementById("playerToggle").innerHTML = "►";
+  }
+}
+
+// Displays currently playing song by AJAX GET
+function nowPlaying() {
   // Located on Testament's stream client 'web' folder
   var url = 'http://cadenceradio.com:8000/now-playing.xsl';
 
@@ -21,58 +70,6 @@ function radioTitle() {
       document.getElementById("song_title").innerHTML = "";
     }
   });
-}
-
-// Update now playing info at an interval
-$(document).ready(function () {
-  
-});
-
-$(document).ready(function () {
-  // Get the localstorage volume or set to 77%
-  setVolume();
-
-  // Play/pause button
-  document.getElementById("playerToggle").addEventListener('click', function(){
-    streamToggle();
-  });
-
-  // Refresh radio title at an interval
-  setTimeout(function () {
-    radioTitle();
-  }, 0);
-  setInterval(function () {
-    radioTitle();
-  }, 10000);
-});
-
-function setVolume() {
-  // Selects either the localstorage volume or a default value
-  var vol = localStorage.getItem('volumeKey') || 0.77;
-  document.getElementById("volume").value = vol;
-  var volume = document.getElementById("stream");
-  volume.volume = vol;
-}
-
-function streamToggle() {
-  var stream = document.getElementById("stream");
-  var mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent);
-  if (stream.paused) {
-    // Loads up the real stream again if mobile
-    if (mobile) {
-      stream.src = "http://cadenceradio.com:8000/cadence1";
-    }
-    stream.load();
-    stream.play();
-    document.getElementById("playerToggle").innerHTML = "❚❚";
-  } else {
-    // Loads up nothing if mobile
-    if (mobile) {
-      stream.src = "";
-    }
-    stream.load();
-    document.getElementById("playerToggle").innerHTML = "►";
-  }
 }
 
 

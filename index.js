ea57836121640fcb15df10cee00b513a85710ef4
kenellorando <kenellorando@gmail.com>
2017-09-25 23:48:08 -0500

    Added functions for file looping for db population

--- index.js
+++ index.js
@@ -18,6 +18,70 @@ app.use(bodyParser.urlencoded({
 // Point to publicly served files
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Connect to database. Populate.
+MongoClient.connect(DB_URL, function (err, db) {
+  if (err) {
+    return console.log(err);
+  }
+
+  var fs = require('fs');
+  var walkPath = '/home/ken/Music';
+
+  var walk = function (dir, done) {
+    fs.readdir(dir, function (error, list) {
+      if (error) {
+        return done(error);
+      }
+
+      var i = 0;
+      (function next() {
+        var file = list[i++];
+
+        if (!file) {
+          return done(null);
+        }
+
+        file = dir + '/' + file;
+        fs.stat(file, function (error, stat) {
+          if (stat && stat.isDirectory()) {
+            walk(file, function (error) {
+              next();
+            });
+          } else {
+            // do stuff to file here
+            console.log(file);
+            next();
+          }
+        });
+      })();
+    });
+  };
+
+  // optional command line params
+  //      source for walk path
+  process.argv.forEach(function (val, index, array) {
+    if (val.indexOf('source') !== -1) {
+      walkPath = val.split('=')[1];
+    }
+  });
+
+  console.log('-------------------------------------------------------------');
+  console.log('processing...');
+  console.log('-------------------------------------------------------------');
+
+  walk(walkPath, function (error) {
+    if (error) {
+      throw error;
+    } else {
+      console.log('-------------------------------------------------------------');
+      console.log('finished.');
+      console.log('-------------------------------------------------------------');
+    }
+  });
+
+  console.log("Database up to date.");
+  db.close();
+});
 
 
 // Search, directed from aria.js AJAX
@@ -27,7 +91,7 @@ app.post('/search', function (req, res) {
   // Received: {"search":"railgun"}
   console.log(req.body.search);
 
-  // Database connect
+  // Database search
   MongoClient.connect(DB_URL, function (err, db) {
     if (err) {
       return console.log(err);

2f902397268e4cc6f78412fd40d11f40736cff58
kenellorando <kenellorando@gmail.com>
2017-09-30 00:31:32 -0500

    Add song path into a data attribute
    Former-commit-id: 41fc86f3f6149985defd66fc9ef4f1bbbcf42988

--- public/js/aria.js
+++ public/js/aria.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
       dataType: "json",
       success: function (data) {
         console.log("Success");
-        console.log("=================")
+        console.log("=================");
         let i = 1;
 
         // Create the container table
@@ -24,14 +24,14 @@ $(document).ready(function () {
 
         if (data.length !== 0) {
           table += "<tr><th>Title</th><th>Artist</th><th>Availability</th></tr>"
-          
+
           data.forEach(function (song) {
             console.log("RESULT " + i)
             console.log("Title: " + song.title);
             console.log("Artist(s): " + song.artist);
             console.log("Album: " + song.album);
             i++;
-            console.log("=================")
+            console.log("=================");
 
             table += "<tr><td id='dataTitle'>" + song.title + "</td><td id='dataArtist'>" + song.artist + "</td><td id='dataRequest'><button id='requestButton'>REQUEST</button></td></tr>";
           })
@@ -49,4 +49,52 @@ $(document).ready(function () {
       }
     });
   });
-})
+
+  $('#searchButton').click(function (e) {
+    
+        // Create a key 'search' to send in JSON
+        var data = {};
+        data.search = $('#searchInput').val();
+    
+        $.ajax({
+          type: 'POST',
+          url: 'http://cadenceradio.com/search',
+          dataType: 'application/json',
+          data: data,
+          dataType: "json",
+          success: function (data) {
+            console.log("Success");
+            console.log("=================");
+            let i = 1;
+    
+            // Create the container table
+            var table = "<table id = 'searchResults'>";
+    
+            if (data.length !== 0) {
+              table += "<tr><th>Title</th><th>Artist</th><th>Availability</th></tr>"
+    
+              data.forEach(function (song) {
+                console.log("RESULT " + i)
+                console.log("Title: " + song.title);
+                console.log("Artist(s): " + song.artist);
+                console.log("Album: " + song.album);
+                i++;
+                console.log("=================");
+    
+                table += "<tr><td id='dataTitle'>" + song.title + "</td><td id='dataArtist'>" + song.artist + "</td><td id='dataRequest'><button id='requestButton' data-path="+song.path+">REQUEST</button></td></tr>";
+              })
+            } else {
+              console.log("No results found. :(");
+            }
+    
+            table += "</table>";
+            // Put table into results html
+            document.getElementById("results").innerHTML = table;
+    
+          },
+          error: function () {
+            console.log("Failure");
+          }
+        });
+      });
+});

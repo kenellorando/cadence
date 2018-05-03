11f10485bf2dfe93c0051ef96b5510c26c39012c
Ryan Hodin <ryan951357@gmail.com>
2018-05-03 04:05:18 -0500

    Add LoggingCallback as an example
    This way, we have an example in the future of how to declare new callback
    types
    
    Also, remove the attempt to be clever in registerCallback. It didn't work.

--- public/js/callback/callbacks.js
+++ public/js/callback/callbacks.js
@@ -3,16 +3,40 @@
 
 // Call this function to register a callback class as inheriting from the CallbackInterface class
 // This is required to ensure compliance with the interfaces used by the theme engine.
-function registerCallback(child) {// Save the current constructor name (so we can access it later)
-    var name=child.prototype.constructor.name
-
-    // Shim function: Call the parent constructor, then the child, in such a way that examining the constructor shows the original type name
-    var shim=eval("(function() {\n\
-       CallbackInterface.apply(this, arguments)\n\
-       \n\
-       "+name+".apply(this, arguments)\n\
-    })")
-
+function registerCallback(child) {
     child.prototype=Object.create(CallbackInterface.prototype)
-    child.prototype.constructor=shim
+    child.prototype.constructor=child
+}
+
+// LoggingCallback: This is an example callback (to show syntax)
+// This callback simply prints messages whenever callback functions are called.
+function LoggingCallback(theme) {
+    CallbackInterface.call(this, theme) // Call the parent constructor. This is highly recommended, but not strictly necessary.
+}
+
+registerCallback(LoggingCallback)
+
+LoggingCallback.prototype.preLoad=function(currentTheme) {
+    console.log("Ready to switch from "+currentTheme.themeKey+" to "+this.theme.themeKey)
+    return false
+}
+
+LoggingCallback.prototype.postLoad=function() {
+    console.log("Switched to "+this.theme.themeKey)
+}
+
+LoggingCallback.prototype.preUnload=function(pendingTheme) {
+    console.log("Ready to switch away from "+this.theme.themeKey+" to "+pendingTheme.themeKey)
+}
+
+LoggingCallback.prototype.postUnload=function () {
+    console.log("Switched away from "+this.theme.themeKey)
+}
+
+LoggingCallback.prototype.nightmodeSwitch=function () {
+    console.log("Switching theme into nightmode...")
+}
+
+LoggingCallback.prototype.daymodeSwitch=function () {
+    console.log("Switching theme into daymode...")
 }

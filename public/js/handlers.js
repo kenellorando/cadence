9692d443f1e9ae342763159d669296844b44934d
Ryan Hodin <ryan951357@gmail.com>
2018-09-18 01:37:39 -0700

    Handle delayed CSS load
    This is better than the original way because it allows these sheets to
    be applied before everything has downloaded.
    
    For context, see https://github.com/kenellorando/cadence/pull/123.

--- public/js/handlers.js
+++ public/js/handlers.js
@@ -2,3 +2,16 @@
  * Handlers for page events for Cadence
  * This file replaces inline handlers in the page with applications in jQuery
  */
+
+// Set up the handlers when the DOM is ready for them
+$(document).ready(function() {
+    // Stylesheet elements which are marked as awaiting load
+    var delayedCss=document.querySelectorAll(".delayedLoad");
+
+    // Iterate over those sheets and set them to be applicable for loading
+    for (var i=0; i<delayedCss.length; ++i) {
+        if (delayedCss[i].media!="all") {
+            delayedCss[i].media="all"
+        }
+    }
+});

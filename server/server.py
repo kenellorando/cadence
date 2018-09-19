4efe10cbd8b4ebbd80ceebc699003a3dd85abb33
Ryan Hodin <ryan951357@gmail.com>
2018-09-18 22:23:35 -0700

    Update canonical default hash
    For the higher-performance timeout default.
    
    This commit fixes #38.
    
    Signed-off-by: Ryan Hodin <ryan951357@gmail.com>

--- server/server.py
+++ server/server.py
@@ -33,7 +33,7 @@ with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'default-conf
     hash = hashlib.sha256(agnostic.encode()).hexdigest()
 
     # This variable holds the 'canonical' hash of the default configuration file
-    canonical = "49a5bae6ba6e4c7ee12c8a4558631e4176e653f81ddc343d9f2ccedfed9c59e4"
+    canonical = "448ca4bec58877468a257756f252e2c137c9bad933515e94d2bc479c3c27fdea"
 
     # Now, the check.
     # Halt startup if the hashes don't match

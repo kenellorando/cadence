d78496325558feb4861014f2b40a5a91e4bb88d2
Ryan Hodin <ryan951357@gmail.com>
2018-09-27 01:59:01 -0500

    Update canonical config hash
    For the new uncaught options
    
    Signed-off-by: Ryan Hodin <ryan951357@gmail.com>

--- server/server.py
+++ server/server.py
@@ -33,7 +33,7 @@ with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'default-conf
     hash = hashlib.sha256(agnostic.encode()).hexdigest()
 
     # This variable holds the 'canonical' hash of the default configuration file
-    canonical = "d3bfbba77e5ee84facd2c0d52f6bc55880bf30224a6a00d42fcbbee6280f806b"
+    canonical = "ec9595100386a656b5e68a6f3f5ffa4ff6e582424d9e1e82dcf3afa8468f98b7"
 
     # Now, the check.
     # Halt startup if the hashes don't match

9d2cf2b770d814ab7120292e90d73c3daf1f0446
kenellorando <kenellorando@gmail.com>
2019-02-17 19:13:25 -0600

    Receive and decode data in ARIA (v1) search and request

--- main.go
+++ main.go
@@ -8,13 +8,20 @@ import (
 )
 
 func main() {
-	c, db := getConfig()
+	// Grab configuration
+	// Start the logger and connect to the database
+	c := getCConfig()
 	initLogger(c.LogLevel)
+
+	// Perform database initialization
+	db := getDBConfig()
 	initDatabase(db)
 
-	// Prepare frontend for serving
-
+	// Handle routes
 	r := mux.NewRouter()
+	// List API routes first
+	r.HandleFunc("/api/aria1/search", ARIA1Search).Methods("POST")
+	r.HandleFunc("/api/aria1/request", ARIA1Request).Methods("POST")
 
 	// Serve other specific routes next
 	r.HandleFunc("/", ServeRoot).Methods("GET")
--- server.go
+++ server.go
@@ -1,18 +1,86 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"path"
+
+	"github.com/kenellorando/clog"
 )
 
 // ServeRoot - serves the frontend root index page
 func ServeRoot(w http.ResponseWriter, r *http.Request) {
+	clog.Info("ServeRoot", fmt.Sprintf("Client %s requesting %s%s", r.RemoteAddr, r.Host, r.URL.Path))
 	w.Header().Set("Content-type", "text/html")
 	http.ServeFile(w, r, path.Dir("./public/index.html"))
 }
 
-// Serve 404 - served for any requests to unknown resources
+// Serve404 - served for any requests to unknown resources
 func Serve404(w http.ResponseWriter, r *http.Request) {
+	clog.Info("Serve404", fmt.Sprintf("Client %s requesting unknown resource %s%s. Returning 404.", r.RemoteAddr, r.Host, r.URL.Path))
 	w.Header().Set("Content-type", "text/html")
 	http.ServeFile(w, r, path.Dir("./public/404/index.html"))
+}
+
+// ARIA1Search - database song searcher
+// Request is received as a value in a raw JSON with key 'search'
+func ARIA1Search(w http.ResponseWriter, r *http.Request) {
+	clog.Debug("ARIA1Search", fmt.Sprintf("Decoding http-request data from client %s.", r.RemoteAddr))
+	// Declare object to hold r body data
+	type Search struct {
+		Query string `json:"search"`
+	}
+	var search Search
+
+	// Decode json object
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		clog.Error("ARIA1Search", fmt.Sprintf("Failed to read http-request body from %s.", r.RemoteAddr), err)
+		return
+	}
+	err = json.Unmarshal(body, &search)
+	if err != nil {
+		clog.Error("ARIA1Search", fmt.Sprintf("Failed to unmarshal http-request body from %s.", r.RemoteAddr), err)
+		return
+	}
+
+	query := search.Query
+	clog.Debug("ARIA1Search", fmt.Sprintf("Search query decoded: '%v'", query))
+	clog.Info("ARIA1Search", fmt.Sprintf("Querying database for: '%v'", query))
+	// Connect to database for query
+	// Query database
+	// Close connection to database
+	// Return data to client
+}
+
+// ARIA1Request - song requester
+func ARIA1Request(w http.ResponseWriter, r *http.Request) {
+	clog.Debug("ARIA1Request", fmt.Sprintf("Decoding http-request data from client %s.", r.RemoteAddr))
+
+	// Declare object to hold r body data
+	type Request struct {
+		Path string `json:"path"`
+	}
+	var request Request
+
+	// Decode json object
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		clog.Error("ARIA1Request", fmt.Sprintf("Failed to read http-request body from %s.", r.RemoteAddr), err)
+		return
+	}
+	err = json.Unmarshal(body, &request)
+	if err != nil {
+		clog.Error("ARIA1Request", fmt.Sprintf("Failed to unmarshal http-request body from %s.", r.RemoteAddr), err)
+		return
+	}
+
+	path := request.Path
+	clog.Debug("ARIA1Request", fmt.Sprintf("Song request decoded: '%v'", path))
+	clog.Info("ARIA1Request", "Connecting to liquidsoap service...")
+	// Telnet to liquidsoap
+	// Forward path in a request command
+	// Disconnect from liquidsoap
 }

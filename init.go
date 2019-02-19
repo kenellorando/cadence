d357fdc20df4e4e24aa1f4b684a814abe57c41ab
kenellorando <kenellorando@gmail.com>
2019-02-18 19:17:14 -0600

    Fix init's logging start

--- init.go
+++ init.go
@@ -31,22 +31,23 @@ type DBConfig struct {
 
 // Grabs all environment variable configurations
 func getConfigs() Config {
+	clog.Info("getConfig", "Getting configuration")
 	// Full config object
 	config := Config{}
 
 	// Get server-related configs
 	server := CConfig{}
-	server.Port = env.GetString("CSERVER_WEB_PORT")
-	server.MusicDir = env.GetString("CSERVER_MUSIC_DIR")
+	server.Port = env.GetString("CSERVER_WEB_PORT", ":8000")
+	server.MusicDir = env.GetString("CSERVER_MUSIC_DIR", "/Default/Fake/Music/Dir")
 	config.server = server
 
 	// Get database related configs
 	db := DBConfig{}
-	db.Host = env.GetString("CSERVER_DB_HOST")
-	db.Port = env.GetString("CSERVER_DB_PORT")
-	db.User = env.GetString("CSERVER_DB_USER")
-	db.Pass = env.GetString("CSERVER_DB_PASS")
-	db.Name = env.GetString("CSERVER_DB_NAME")
+	db.Host = env.GetString("CSERVER_DB_HOST", "Default_FakeDBHost")
+	db.Port = env.GetString("CSERVER_DB_PORT", "Default_FakeDBPort")
+	db.User = env.GetString("CSERVER_DB_USER", "Default_FakeDBUser")
+	db.Pass = env.GetString("CSERVER_DB_PASS", "Default_FakeDBPass")
+	db.Name = env.GetString("CSERVER_DB_NAME", "Default_FakeDBName")
 	config.db = db
 
 	return config
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 
 	env "github.com/deanishe/go-env"
 	"github.com/gorilla/mux"
@@ -14,23 +15,21 @@ import (
 func init() {
 	// Init defaults are declared here
 	// Although all configurations are set into environment variables
-	// We camn declare some defaults here for server initialization purposes
-	const defaultLogLevel = 4
+	// We can declare some defaults here for server initialization purposes
+	const defaultLogLevel = 5
 
-	// Todo:
-	// make a reusable function to check if a value in the environment variable is blank
-
-	// Set logging to log level
-	logLevel := env.GetInt("CSERVER_LOGLEVEL")
-	logPointer := &logLevel
-	// If no log level is set, default to a value
-	// Else, send the value
-	if logPointer == nil {
-		clog.Init(4)
-		clog.Info("init", fmt.Sprintf("No default logging level was found. Using default level 4"))
+	// Set logging level
+	// If there is no data, default to defaultLogLevel
+	// Else, use the value.
+	logLevel := os.Getenv("CSERVER_LOGLEVEL")
+	if len(logLevel) == 0 {
+		clog.Info("init", fmt.Sprintf("No default logging level was found."))
+		clog.Init(defaultLogLevel)
+		clog.Info("init", fmt.Sprintf("Logging level set to hardcoded default level <%v>", defaultLogLevel))
 	} else {
+		logLevel := env.GetInt("CSERVER_WEB_PORT")
 		clog.Init(logLevel)
-		clog.Info("init", fmt.Sprintf("Setting logging service verbosity to <%v>", logLevel))
+		clog.Info("init", fmt.Sprintf("Set logging verbosity to <%v>", logLevel))
 	}
 }
 

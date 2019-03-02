98c2c2b54cf6b46834b59679599e02f66019a8e0
kenellorando <kenellorando@gmail.com>
2019-03-02 01:21:08 -0600

    Implement templating for nav and footer

--- public/js/views.js
+++ public/js/views.js
@@ -1,5 +1,22 @@
 // Switch the display of the front matter
 $(document).ready(function () {
+    // Load this template for the navigation bar
+    document.getElementById("nav").innerHTML = `
+<span id="navLeft">
+    <a id="banner">CADENCE</a>
+</span>
+<span id="navRight">
+    <a id="navRequest">Request</a>
+    <a id="navLibrary">Library</a>
+    <a href="https://github.com/kenellorando/cadence" target="_blank">Source</a>
+</span>
+`
+
+    // Load this template for the page footer
+    document.getElementById("footer").innerHTML = `
+<span>A project by <a href="https://kenellorando.com/">Ken Ellorando</a></span>
+`
+
     document.getElementById("banner").addEventListener('click', function(){
         document.getElementById("frontMain").style.display = "block";
         document.getElementById("frontRequest").style.display = "none";

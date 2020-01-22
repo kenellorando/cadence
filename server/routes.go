217a70375001ee30af7f89990edbbca19abeb970
Ryan Hodin <ryan951357@gmail.com>
2020-01-21 23:25:06 -0600

    Import search syntax from v3 server
    These searches have been directly ported from Python, and I'm not
    entirely sure if I did so correctly, so some review and testing is
    necessary.
    
    Once this is fixed up, the general search we have now will be improved
    to return results in order of how similar they are to the search term.
    
    Additionally, there will be a number of ways to express that you are
    searching for a specific field in natural-ish ways: For example, you
    could search for "rock songs", or "songs released in 2015", or "songs
    by fripside". Results for these will be more specific because each will
    use only one field - Two of which aren't used in the general search -
    and they will also be sorted by similarity to the search term.

--- server/routes.go
+++ server/routes.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -11,6 +12,15 @@ import (
 
 	"github.com/kenellorando/clog"
 )
+
+// Utility functions
+func startsWith(str string, prefix string) bool {
+	return len(str) >= len(prefix) && str[:len(prefix)] == prefix
+}
+
+func endsWith(str string, suffix string) bool {
+	return len(str) >= len(suffix) && str[len(s)-len(suffix):] == suffix
+}
 
 // ServeRoot - serves the frontend root index page
 func ServeRoot(w http.ResponseWriter, r *http.Request) {
@@ -50,19 +60,85 @@ func ARIA1Search(w http.ResponseWriter, r *http.Request) {
 
 	// Query database
 	selectStatement := fmt.Sprintf("SELECT \"id\", \"artist\", \"title\" FROM %s ", c.schema.Table)
-	selectWhereStatement := fmt.Sprintf(selectStatement+"WHERE title LIKE '%%%s%%' OR artist LIKE '%%%s%%'", query, query)
+	var rows *sql.Rows
+
+	// Decide based on the format of the query if this is a special form.
+	// The available fields are : "title", "album", "artist", "genre", "year"
+	if startsWith(query, "songs named ") {
+		// Title search
+		q := query[len("songs named "):]
+		selectWhereStatement := selectStatement + "WHERE title ILIKE $1 ORDER BY levenshtein($2, title) ASC"
+		rows, err := database.Query(selectWhereStatement, "%"+q+"%", q)
+		if err != nil {
+			clog.Error("ARIA1Search", "Database search failed.", err)
+			return
+		}
+	} else if startsWith(query, "songs by ") {
+		// Artist search
+		q := query[len("songs by "):]
+		selectWhereStatement := selectStatement + "WHERE artist ILIKE $1 ORDER BY levenshtein($2, artist) ASC"
+		rows, err := database.Query(selectWhereStatement, "%"+q+"%", q)
+		if err != nil {
+			clog.Error("ARIA1Search", "Database search failed.", err)
+			return
+		}
+	} else if endsWith(query, " songs") {
+		// Genre search
+		q := query[:len(query)-len(" songs")]
+		selectWhereStatement := selectStatement + "WHERE genre ILIKE $1 ORDER BY levenshtein($2, genre) ASC"
+		rows, err := database.Query(selectWhereStatement, "%"+q+"%", q)
+		if err != nil {
+			clog.Error("ARIA1Search", "Database search failed.", err)
+			return
+		}
+	} else if startsWith(query, "songs from ") {
+		// Joint year/album search
+		// Note that the year query doesn't use includes: "Songs from 20" shouldn't return a song made in 2009.
+		q := query[len("songs from "):]
+		selectWhereStatement := selectStatement + "WHERE year LIKE $1 OR ALBUM ILIKE $2 ORDER BY LEAST(levenshtein($3, year), levenshtein($4, album)) ASC"
+		rows, err := database.Query(selectWhereStatement, q, "%"+q+"%", q, q)
+		if err != nil {
+			clog.Error("ARIA1Search", "Database search failed.", err)
+			return
+		}
+	} else if startsWith(query, "songs released in ") {
+		// Year search
+		// This search also doesn't use an include-style parameter
+		q := query[len("songs released in "):]
+		selectWhereStatement := selectStatement + "WHERE year LIKE $1 ORDER BY levenshtein($2, year) ASC"
+		rows, err := database.Query(selectWhereStatement, q, q)
+		if err != nil {
+			clog.Error("ARIA1Search", "Database search failed.", err)
+			return
+		}
+	} else if startsWith(query, "songs in ") {
+		// Album search
+		q := query[len("songs in "):]
+		selectWhereStatement := selectStatement + "WHERE album ILIKE $1 ORDER BY levenshtein($2, album) ASC"
+		rows, err := database.Query(selectWhereStatement, "%"+q+"%", q)
+		if err != nil {
+			clog.Error("ARIA1Search", "Database search failed.", err)
+			return
+		}
+	} else {
+		// After all that work, we've concluded we don't have a special form.
+		// It's been an open question since before v3.0 what exactly we should do for a general search...
+		// But, it's always been the case that either title or artist search works here.
+		selectWhereStatement := selectStatement + "WHERE artist ILIKE $1 OR title ILIKE $2 ORDER BY LEAST(levenshtein($3, artist), levenshtein($4, title)) ASC"
+		rows, err := database.Query(selectWhereStatement, "%"+q+"%", "%"+q+"%", q, q)
+		if err != nil {
+			clog.Error("ARIA1Search", "Database search failed.", err)
+			return
+		}
+	}
+
 	// Declare object for a song
 	type SongData struct {
 		ID     int
 		Artist string
 		Title  string
 	}
-	// Run the search query on the database
-	rows, err := database.Query(selectWhereStatement)
-	if err != nil {
-		clog.Error("ARIA1Search", "Database search failed.", err)
-		return
-	}
+
 	// Scan the returned data and save the relevant info
 	clog.Debug("ARIA1Search", "Scanning returned data...")
 	var searchResults []SongData

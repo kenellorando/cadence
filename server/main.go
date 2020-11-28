23c51a8ab3f52ff585509bb81170e4538c9eeb5a
kenellorando <kenellorando@gmail.com>
2020-11-27 20:46:48 -0500

    Create a whitelist check
    Commit creates an ARIA-2 API route for requests. This new route performs a check on a json request field called "Token" and searches a local whitelist text file for a match. If there is a match, it bypasses the usual rate-limiter.

--- server/main.go
+++ server/main.go
@@ -123,10 +123,14 @@ func main() {
 		s.PathPrefix("/").Handler(http.FileServer(http.Dir("./public/docs"))).Methods("GET")
 	*/
 
-	// List API routes firstther specific routes next
+	// List API routes first
 	r.HandleFunc("/api/aria1/search", ARIA1Search).Methods("POST")
 	r.HandleFunc("/api/aria1/request", ARIA1Request).Methods("POST")
 	r.HandleFunc("/api/aria1/library", ARIA1Library).Methods("GET")
+
+	// Aria2
+	r.HandleFunc("/api/aria2/request", ARIA2Request).Methods("POST")
+
 	// Serve other specific routes next
 	r.HandleFunc("/", ServeRoot).Methods("GET")
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./public/static/"))))

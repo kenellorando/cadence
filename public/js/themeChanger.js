7be654f3472b60e517068e0212ddb3bd0ea09180
kenellorando <kenellorando@gmail.com>
2021-03-03 19:23:24 -0500

    Beautify

--- public/js/themeChanger.js
+++ public/js/themeChanger.js
@@ -1,40 +1,36 @@
 // themeButton click handling
 $(document).ready(function() {
-    $('.themeButton').on('click', function() {
-        var themeChoice = $(this).attr('id');
-        themeChanger(themeChoice);
-    });
+	$('.themeButton').on('click', function() {
+		var themeChoice = $(this).attr('id');
+		themeChanger(themeChoice);
+	});
 });
-
 // Change theme
 function themeChanger(themeChoice) {
-    // theme object is from themeMetadata.js
-    var targetTheme = theme[themeChoice];
-    document.getElementById("themeStylesheet").href = targetTheme.css;
-    localStorage.setItem('themeKey', targetTheme.key);
-    colorButton(themeChoice);
+	// theme object is from themeMetadata.js
+	var targetTheme = theme[themeChoice];
+	document.getElementById("themeStylesheet").href = targetTheme.css;
+	localStorage.setItem('themeKey', targetTheme.key);
+	colorButton(themeChoice);
 }
-
 // Theme button functionality
 function colorButton(themeChoice) {
-    // Change all theme buttons colors to the inactive style
-    var buttons = document.getElementsByClassName("themeButton");
-    for(var i =0, il = buttons.length;i<il;i++){
-       buttons[i].classList.remove("activeTheme");
-       buttons[i].classList.add("inactiveTheme");
-    }
-
-    // Override the selected theme button the active style
-    document.getElementById(themeChoice).classList.remove("inactiveTheme");
-    document.getElementById(themeChoice).classList.add("activeTheme");
+	// Change all theme buttons colors to the inactive style
+	var buttons = document.getElementsByClassName("themeButton");
+	for (var i = 0, il = buttons.length; i < il; i++) {
+		buttons[i].classList.remove("activeTheme");
+		buttons[i].classList.add("inactiveTheme");
+	}
+	// Override the selected theme button the active style
+	document.getElementById(themeChoice).classList.remove("inactiveTheme");
+	document.getElementById(themeChoice).classList.add("activeTheme");
 }
-
 // Retrieve themeKey in localStoprage
 $(document).ready(function() {
-    var themeKey = localStorage.getItem('themeKey');
-    if (themeKey === null) {
-        themeChanger("Default");
-    } else {
-        themeChanger(themeKey);
-    }
+	var themeKey = localStorage.getItem('themeKey');
+	if (themeKey === null) {
+		themeChanger("Default");
+	} else {
+		themeChanger(themeKey);
+	}
 });

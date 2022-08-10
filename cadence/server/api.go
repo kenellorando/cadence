e1ca495152ecf26688326471a73249da7e15f5d2
kenellorando <kenellorando@gmail.com>
2022-08-09 19:30:51 -0500

    Refactor 4 - request

--- cadence/server/api.go
+++ cadence/server/api.go
@@ -7,12 +7,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"path"
-	"strings"
 	"time"
 
 	"github.com/Jeffail/gabs"
@@ -21,10 +19,6 @@ import (
 	"github.com/kenellorando/clog"
 )
 
-// Map of requester IPs to be locked out of making requests
-var requestTimeoutIPs = make(map[string]int)
-
-// Fileservers ////////////////////////////////////////////////////////////////////
 func SiteRoot() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		clog.Info("ServeRoot", fmt.Sprintf("Client %s requesting %s%s", r.Header.Get("X-Forwarded-For"), r.Host, r.URL.Path))
@@ -43,7 +37,7 @@ func Site404() http.HandlerFunc {
 	}
 }
 
-// Default API ////////////////////////////////////////////////////////////////////
+// /api/search
 func Search() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		clog.Info("Search", fmt.Sprintf("Search by client %s.", r.Header.Get("X-Forwarded-For")))
@@ -56,12 +50,14 @@ func Search() http.HandlerFunc {
 		decoder := json.NewDecoder(r.Body)
 		err := decoder.Decode(&search)
 		if err != nil {
+			w.WriteHeader(http.StatusBadRequest) // 400 Bad Request
 			return
 		}
 
-		queryResults, err := dbQuery(search.Query)
+		queryResults, err := searchByQuery(search.Query)
 		if err != nil {
-
+			w.WriteHeader(http.StatusInternalServerError) // 500 Internal Server Error
+			return
 		}
 
 		jsonMarshal, _ := json.Marshal(queryResults)
@@ -71,6 +67,7 @@ func Search() http.HandlerFunc {
 	}
 }
 
+// /api/request/id
 func RequestID() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		clog.Info("Request", fmt.Sprintf("Request-by-ID by client %s.", r.Header.Get("X-Forwarded-For")))
@@ -87,7 +84,7 @@ func RequestID() http.HandlerFunc {
 			return
 		}
 
-		path, err := dbQueryPath(request.ID)
+		path, err := getPathById(request.ID)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError) // 500 Internal Server Error
 			return
@@ -102,6 +99,7 @@ func RequestID() http.HandlerFunc {
 	}
 }
 
+// /api/request/bestmatch
 func RequestBestMatch() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		clog.Debug("Search", fmt.Sprintf("Decoding http-request data from client %s.", r.Header.Get("X-Forwarded-For")))
@@ -118,12 +116,12 @@ func RequestBestMatch() http.HandlerFunc {
 			return
 		}
 
-		queryResults, err := dbQuery(rbm.Query)
+		queryResults, err := searchByQuery(rbm.Query)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError) // 500 Internal Server Error
 			return
 		}
-		path, err := dbQueryPath(queryResults[0].ID)
+		path, err := getPathById(queryResults[0].ID)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError) // 500 Internal Server Error
 			return
@@ -137,44 +135,22 @@ func RequestBestMatch() http.HandlerFunc {
 	}
 }
 
+// /api/nowplaying/metadata
 func NowPlayingMetadata() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// Since Cadence does maintain state on what the stream server is playing
-		// it needs to check first for the simple data the stream server provides.
-		clog.Debug("NowPlaying", fmt.Sprintf("Client %s requesting %s%s", r.Header.Get("X-Forwarded-For"), r.Host, r.URL.Path))
-
-		resp, err := http.Get("http://icecast2:8000/status-json.xsl")
+		title, artist, err := getNowPlaying()
 		if err != nil {
-			clog.Error("NowPlaying", "Failed to connect to audio stream server.", err)
+			w.WriteHeader(http.StatusInternalServerError) // 500 Internal Server Error
 			return
 		}
-		defer resp.Body.Close()
-		if resp.StatusCode != http.StatusOK {
-			clog.Error("NowPlaying", "Audio stream server returned bad status", err)
-			return
-		}
-
-		body, _ := io.ReadAll(resp.Body)
-		jsonParsed, _ := gabs.ParseJSON([]byte(body))
-
-		var artist, _ = jsonParsed.Path("icestats.source.artist").Data().(string)
-		var title, _ = jsonParsed.Path("icestats.source.title").Data().(string)
-
-		clog.Debug("NowPlayingMetadata", fmt.Sprintf("The stream server reports it is now playing: '%s' by '%s'.", title, artist))
-
-		// When the title and artist are known, we query the metadata DB for all of the data it has on the playing track
-		selectStatement := fmt.Sprintf("SELECT rowid,artist,title,album,genre,year FROM %s WHERE artist=\"%v\" AND title=\"%v\";", c.MetadataTable, artist, title)
-		rows, err := db.Query(selectStatement)
+		queryResults, err := searchByTitleArtist(title, artist)
 		if err != nil {
-			clog.Error("NowPlayingMetadata", "Could not query DB.", err)
+			w.WriteHeader(http.StatusInternalServerError) // 500 Internal Server Error
 			return
 		}
-		if err != nil {
-			clog.Error("NowPlayingMetadata", "Could not query the DB.", err)
-			return
-		}
+		song := queryResults[0]
 
-		// Declare object for a song
+		// Return data to client
 		type SongData struct {
 			ID     int
 			Artist string
@@ -183,21 +159,10 @@ func NowPlayingMetadata() http.HandlerFunc {
 			Genre  string
 			Year   int
 		}
-
-		clog.Debug("NowPlayingMetadata", "Scanning returned data...")
-		song := new(SongData)
-		for rows.Next() {
-			err := rows.Scan(&song.ID, &song.Artist, &song.Title, &song.Album, &song.Genre, &song.Year)
-			if err != nil {
-				clog.Error("NowPlayingMetadata", "Data scan failed.", err)
-				return
-			}
-		}
 		result := SongData{ID: song.ID, Artist: song.Artist, Title: song.Title, Album: song.Album, Genre: song.Genre, Year: song.Year}
-
-		// Return data to client
 		jsonMarshal, _ := json.Marshal(result)
 		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK) // 200 OK
 		w.Write(jsonMarshal)
 	}
 }
@@ -205,107 +170,69 @@ func NowPlayingMetadata() http.HandlerFunc {
 // /api/nowplaying/albumart
 func NowPlayingAlbumArt() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// Since Cadence does maintain state on what the stream server is playing
-		// it needs to check first for the simple data the stream server provides.
-		clog.Debug("NowPlayingAlbumArt", fmt.Sprintf("Client %s requesting %s%s", r.Header.Get("X-Forwarded-For"), r.Host, r.URL.Path))
-
-		resp, err := http.Get("http://" + c.StreamAddress + c.StreamPort + "/status-json.xsl")
+		title, artist, err := getNowPlaying()
 		if err != nil {
-			clog.Error("NowPlayingAlbumArt", "Failed to connect to audio stream server.", err)
+			w.WriteHeader(http.StatusInternalServerError) // 500 Internal Server Error
 			return
 		}
-		defer resp.Body.Close()
-		if resp.StatusCode != http.StatusOK {
-			clog.Error("NowPlayingAlbumArt", "Audio stream server returned bad status", err)
+		queryResults, err := searchByTitleArtist(title, artist)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError) // 500 Internal Server Error
 			return
 		}
-
-		body, err := io.ReadAll(resp.Body)
+		path, err := getPathById(queryResults[0].ID)
 		if err != nil {
-			clog.Error("NowPlayingAlbumArt", "error", err)
-		}
-		jsonParsed, err := gabs.ParseJSON([]byte(body))
-		if err != nil {
-			clog.Error("NowPlayingAlbumArt", "error", err)
-		}
-
-		var artist, _ = jsonParsed.Path("icestats.source.artist").Data().(string)
-		var title, _ = jsonParsed.Path("icestats.source.title").Data().(string)
-
-		clog.Debug("NowPlayingAlbumArt", fmt.Sprintf("The stream server reports it is now playing: '%s' by '%s'.", title, artist))
-
-		log.Printf("%s %s", artist, title)
-
-		selectStatement := fmt.Sprintf("SELECT path FROM %s WHERE artist=\"%v\" AND title=\"%v\";", c.MetadataTable, artist, title)
-		rows, err := db.Query(selectStatement)
-		if err != nil {
-			clog.Error("NowPlayingAlbumArt", "Could not query the DB for a path.", err)
-			return
-		}
-		if err != nil {
-			clog.Error("NowPlayingAlbumArt", "Could not query the DB for a path.", err)
+			w.WriteHeader(http.StatusInternalServerError) // 500 Internal Server Error
 			return
 		}
 
-		var pic []byte
-
-		for rows.Next() {
-			var path string
-			err := rows.Scan(&path)
-			if err != nil {
-				clog.Debug("NowPlayingAlbumArt", "ae")
-				return
-			}
-			// Open a file for reading
-			file, err := os.Open(path)
-			if err != nil {
-				clog.Error("NowPlayingAlbumArt", "Could not open music file for album art.", err)
-				return
-			}
-			// Read metadata from the file
-			tags, err := tag.ReadFrom(file)
-			if err != nil {
-				clog.Error("NowPlayingAlbumArt", "Could not read tags from file for album art.", err)
-				return
-			}
-			pic = tags.Picture().Data
+		file, err := os.Open(path)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError) // 500 Internal Server Error
+			return
 		}
-		// Declare object for a song
+		// Read metadata from the file
+		tags, err := tag.ReadFrom(file)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError) // 500 Internal Server Error
+			return
+		}
+
+		// Return data to client
 		type SongData struct {
 			Picture []byte
 		}
-		result := SongData{Picture: pic}
-
-		// Return data to client
+		result := SongData{Picture: tags.Picture().Data}
 		jsonMarshal, _ := json.Marshal(result)
 		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK) // 200 OK
 		w.Write(jsonMarshal)
 	}
 }
 
+// /api/version
 func Version() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		clog.Debug("Version", fmt.Sprintf("Client %s requesting %s%s", r.Header.Get("X-Forwarded-For"), r.Host, r.URL.Path))
-
 		// Return data to client
 		type CadenceVersion struct {
 			Version string
 		}
 		version := CadenceVersion{Version: c.Version}
 		jsonMarshal, _ := json.Marshal(version)
-
-		w.WriteHeader(http.StatusOK) // 200 Accepted
 		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK) // 200 OK
 		w.Write(jsonMarshal)
 	}
 }
 
-// Status Checks ////////////////////////////////////////////////////////////////////
+// /ready
 func Ready() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK) // 200 Accepted
+		w.WriteHeader(http.StatusOK) // 200 OK
 	}
 }
+
+/////////////////
 
 var upgrader = websocket.Upgrader{}
 
@@ -409,27 +336,4 @@ func RadioData() http.HandlerFunc {
 			time.Sleep(1 * time.Second)
 		}
 	}
-}
-
-func tokenCheck(token string) bool {
-	clog.Info("tokenCheck", fmt.Sprintf("Checking token %s...", token))
-
-	if len(token) != 26 {
-		clog.Debug("tokenCheck", fmt.Sprintf("Token %s does not satisfy length requirements.", token))
-		return false
-	}
-
-	// Check the whitelist. If this fails, the whitelist is not configured. No panic is thrown, but the bypass is denied.
-	b, err := ioutil.ReadFile(c.WhitelistPath)
-	if err != nil {
-		return false
-	}
-	s := string(b)
-
-	if strings.Contains(s, token) {
-		clog.Info("tokenCheck", fmt.Sprintf("Token %s is valid.", token))
-		return true
-	}
-	clog.Info("tokenCheck", fmt.Sprintf("Token %s is invalid.", token))
-	return false
 }
--- cadence/server/api_actions.go
+++ cadence/server/api_actions.go
@@ -1,12 +1,17 @@
 // api_actions.go
 // Repeatable actions used by API functions. Mostly database and audio client functions.
+// No responses to clients here.
+
 package main
 
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"net"
+	"net/http"
 
+	"github.com/Jeffail/gabs"
 	"github.com/kenellorando/clog"
 )
 
@@ -20,8 +25,9 @@ type SongData struct {
 	Year   int
 }
 
-// Takes a search string, returns a slice of SongData of songs by relevance.
-func dbQuery(query string) (queryResults []SongData, err error) {
+// Takes a query string to search the database.
+// Returns a slice of SongData of songs by relevance.
+func searchByQuery(query string) (queryResults []SongData, err error) {
 	clog.Info("dbQuery", fmt.Sprintf("Searching database for query: '%v'", query))
 
 	selectWhereStatement := fmt.Sprintf("SELECT \"rowid\", \"artist\", \"title\",\"album\", \"genre\", \"year\" FROM %s ", c.MetadataTable) + "WHERE artist LIKE $1 OR title LIKE $2 ORDER BY rank"
@@ -36,7 +42,7 @@ func dbQuery(query string) (queryResults []SongData, err error) {
 		err = rows.Scan(&song.ID, &song.Artist, &song.Title, &song.Album, &song.Genre, &song.Year)
 		if err != nil {
 			clog.Error("Search", "Data scan failed.", err)
-			return
+			continue
 		}
 		queryResults = append(queryResults, SongData{ID: song.ID, Artist: song.Artist, Title: song.Title, Album: song.Album, Genre: song.Genre, Year: song.Year})
 	}
@@ -44,9 +50,32 @@ func dbQuery(query string) (queryResults []SongData, err error) {
 	return queryResults, nil
 }
 
-// Takes a song ID integer, returns a string absolute path of the song.
-// Separated in order to keep filesystem paths internal.
-func dbQueryPath(id int) (path string, err error) {
+// Takes a title and artist string to find exact matches for both.
+// Returns a slice of SongData of songs by relevance.
+// This search should only have one result unless multiple audio files share the exact same title and artist.
+func searchByTitleArtist(title string, artist string) (queryResults []SongData, err error) {
+	selectStatement := fmt.Sprintf("SELECT rowid,artist,title,album,genre,year FROM %s WHERE title=\"%v\" AND artist=\"%v\";", c.MetadataTable, artist, title)
+	rows, err := db.Query(selectStatement)
+	if err != nil {
+		clog.Error("NowPlayingMetadata", "Could not query DB.", err)
+		return nil, err
+	}
+
+	for rows.Next() {
+		song := new(SongData)
+		err = rows.Scan(&song.ID, &song.Artist, &song.Title, &song.Album, &song.Genre, &song.Year)
+		if err != nil {
+			clog.Error("Search", "Data scan failed.", err)
+			continue
+		}
+		queryResults = append(queryResults, SongData{ID: song.ID, Artist: song.Artist, Title: song.Title, Album: song.Album, Genre: song.Genre, Year: song.Year})
+	}
+	return queryResults, nil
+}
+
+// Takes a song ID integer.
+// Returns a string absolute path of the song.
+func getPathById(id int) (path string, err error) {
 	clog.Info("dbQuery", fmt.Sprintf("Searching database for the path of song: '%v'", id))
 
 	selectWhereStatement := fmt.Sprintf("SELECT \"path\" FROM %s WHERE rowid=%v", c.MetadataTable, id)
@@ -88,4 +117,34 @@ func pushRequest(path string) (message string, err error) {
 	fmt.Fprintf(conn, "quit"+"\n")
 
 	return message, nil
+}
+
+// Takes no arguments.
+// Returns the title and artist strings actively playing on Icecast.
+func getNowPlaying() (title string, artist string, err error) {
+	resp, err := http.Get("http://" + c.StreamAddress + c.StreamPort + "/status-json.xsl")
+	if err != nil {
+		clog.Error("getNowPlaying", "Failed to connect to audio stream server.", err)
+		return "", "", err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		clog.Error("getNowPlaying", "Audio stream server returned bad status", err)
+		return "", "", err
+	}
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		clog.Error("getNowPlaying", "error", err)
+		return "", "", err
+	}
+	jsonParsed, err := gabs.ParseJSON([]byte(body))
+	if err != nil {
+		clog.Error("getNowPlaying", "error", err)
+		return "", "", err
+	}
+
+	title, _ = jsonParsed.Path("icestats.source.title").Data().(string)
+	artist, _ = jsonParsed.Path("icestats.source.artist").Data().(string)
+	clog.Info("getNowPlaying", fmt.Sprintf("The stream server reports it is playing: '%s' by '%s'.", title, artist))
+	return title, artist, nil
 }

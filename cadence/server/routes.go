9f2c036cb8b4688900b342c5456fbf0daa951f51
kenellorando <kenellorando@gmail.com>
2022-08-15 00:22:22 -0500

    Create new rate limiter

--- cadence/server/routes.go
+++ cadence/server/routes.go
@@ -1,21 +1,49 @@
 // routes.go
-// Just the router
+// Rate limiter and router
 
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 
+	"github.com/kenellorando/clog"
 	"gopkg.in/antage/eventsource.v1"
 )
+
+var limit_map = make(map[string]time.Time)
+
+func rateLimit(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ip := r.RemoteAddr
+		expiration, ok := limit_map[ip]
+
+		if !ok {
+			clog.Debug("rateLimit", fmt.Sprintf("<%s> is a new IP.", ip))
+			limit_map[ip] = time.Now().Add(time.Second * time.Duration(c.RequestRateLimit))
+			next.ServeHTTP(w, r)
+		} else {
+			if expiration.Before(time.Now()) {
+				clog.Debug("rateLimit", fmt.Sprintf("<%s> is an existing IP that is no longer rate limited.", ip))
+				limit_map[ip] = time.Now().Add(time.Second * time.Duration(c.RequestRateLimit))
+				next.ServeHTTP(w, r)
+			} else {
+				clog.Debug("rateLimit", fmt.Sprintf("<%s> is rate limited.", ip))
+				w.WriteHeader(http.StatusTooManyRequests) // 429 Too Many Requests
+				return
+			}
+		}
+	})
+}
 
 var radiodata_sse = eventsource.New(nil, nil)
 
 func routes() *http.ServeMux {
 	r := http.NewServeMux()
 	r.Handle("/api/search", Search())
-	r.Handle("/api/request/id", RequestID())
-	r.Handle("/api/request/bestmatch", RequestBestMatch())
+	r.Handle("/api/request/id", rateLimit(RequestID()))
+	r.Handle("/api/request/bestmatch", rateLimit(RequestBestMatch()))
 	r.Handle("/api/nowplaying/metadata", NowPlayingMetadata())
 	r.Handle("/api/nowplaying/albumart", NowPlayingAlbumArt())
 	r.Handle("/api/listenurl", ListenURL())

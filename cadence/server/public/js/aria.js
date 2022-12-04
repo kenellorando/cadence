cf32cfb9e9d7621e7b8f0a2b3de55962c064d017
kenellorando <kenellorando@gmail.com>
2022-12-03 22:22:25 -0600

    Update song table style

--- cadence/server/public/js/aria.js
+++ cadence/server/public/js/aria.js
@@ -135,7 +135,7 @@ function postSearch() {
 				// Build the results table
 				table += "<thead><tr><th>Artist</th><th>Title</th><th>Availability</th></tr></thead><tbody>"
 				data.forEach(function(song) {
-					table += "<tr><td>" + song.Artist + "</td><td>" + song.Title + "</td><td><button class='button requestButton' data-id='" + escape(song.ID) + "'>REQUEST</button></td></tr>";
+					table += "<tr><td>" + song.Artist + "</td><td>" + song.Title + "</td><td><button class='button is-small is-light requestButton' data-id='" + escape(song.ID) + "'>Request</button></td></tr>";
 				})
 				table += "</tbody>"
 

fbb68c1a2acaf852859cc2fd78c0b7ff09301ac6
kenellorando <kenellorando@gmail.com>
2023-01-20 23:32:55 -0600

    Finish small refactor

--- cadence/server/public/js/page.js
+++ cadence/server/public/js/page.js
@@ -1,58 +1,45 @@
 $(document).ready(function() {
-	// Load audio stream source
-	var stream = document.getElementById("stream");
-	document.getElementById("playButton").addEventListener('click', function() {
-		if (stream.paused) {
-			stream.src = streamSrcURL;
-			stream.load();
-			stream.play();
-			// Replace the ❙❙ in the button when playing
-			document.getElementById("playButton").innerHTML = "❙❙";
-		} else {
-			// Clear the audio source
-			stream.src = "";
-			stream.load();
-			stream.pause();
-			// Replace the ► in the button when paused
-			document.getElementById("playButton").innerHTML = "►";
-		}
-	}, true);
-
 	// Warn iOS and Safari users
 	let safariUA = /Apple/i.test(navigator.vendor);
 	let iOSUA = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
 	if (iOSUA || safariUA) {
 		alert("You appear to be using an iOS device or a Safari browser. Cadence stream playback may not be compatible with your platform.")
 	}
+	
+	// Loads and unloads audio stream source
+	document.getElementById("playButton").addEventListener('click', function() {
+		var stream = document.getElementById("stream");
+		if (stream.paused) {
+			stream.src = streamSrcURL;
+			stream.load();
+			stream.play();
+			document.getElementById("playButton").innerHTML = "⏸";
+		} else {
+			stream.src = "";
+			stream.load();
+			stream.pause();
+			document.getElementById("playButton").innerHTML = "⏵";
+		}
+	});
 
-	// Volume control
 	// Load cached volume level, or 30%
-	var vol = localStorage.getItem('volumeKey') || 0.30;
-	document.getElementById("volume").value = vol;
-	// Set active volume on audio stream to loaded value
-	var volume = document.getElementById("stream");
-	volume.volume = vol;
+	var cachedVolume = localStorage.getItem('volumeKey') || 0.30;
+	$("#volume").value = cachedVolume;
+	$("#stream").volume = cachedVolume;
 	// Volume bar listeners
 	$("#volume").change(function() {
-		volumeToggle(this.value);
+		stream.volume = this.value;
+		localStorage.setItem('volumeKey', this.value);
 	}).on("input", function() {
-		volumeToggle(this.value);
+		stream.volume = this.value;
+		localStorage.setItem('volumeKey', this.value);
 	});
-	// Volume control
-	function volumeToggle(vol) {
-		var volume = document.getElementById("stream");
-		volume.volume = vol;
-		// Sets the new set volume into localstorage
-		localStorage.setItem('volumeKey', vol);
-	}
 
 	// Tab switching
 	$('#tabs li').on('click', function() {
 		var tab = $(this).data('tab');
-	
 		$('#tabs li').removeClass('is-active');
 		$(this).addClass('is-active');
-	
 		$('#tab-content section').removeClass('is-active');
 		$('section[data-content="' + tab + '"]').addClass('is-active');
 	});

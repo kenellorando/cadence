5287133f1be02fcf6f85d01bfbb2dbdeb99707b6
Bobby Ton <bobbyt1997@gmail.com>
2023-02-13 22:53:42 -0600

    Update frontend code to utilize es6 syntax

--- cadence/server/public/js/page.js
+++ cadence/server/public/js/page.js
@@ -1,51 +1,49 @@
-$(document).ready(function () {
-	const stream = document.getElementById("stream");
-	const volbar = document.getElementById("volume");
+$(document).ready(() => {
+   const PLAY_BUTTON = 'playButton'
+   const VOLUME_KEY = 'volumeKey'
 
-	// Warn iOS and Safari users
-	let safariUA = /Apple/i.test(navigator.vendor);
-	let iOSUA =
-		/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
-	if (iOSUA || safariUA) {
-		alert(
-			"You appear to be using an iOS device or a Safari browser. Cadence stream playback may not be compatible with your platform."
-		);
-	}
+   const stream = document.getElementById('stream')
+   const volbar = document.getElementById('volume')
 
-	// Loads and unloads audio stream source
-	document
-		.getElementById("playButton")
-		.addEventListener("click", function () {
-			if (stream.paused) {
-				stream.src = streamSrcURL;
-				stream.load();
-				stream.play();
-				document.getElementById("playButton").innerHTML = "⏸";
-			} else {
-				stream.src = "";
-				stream.load();
-				stream.pause();
-				document.getElementById("playButton").innerHTML = "⏵";
-			}
-		});
+   // Warn iOS and Safari users
+   let safariUA = /Apple/i.test(navigator.vendor)
+   let iOSUA = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream
 
-	// Load cached volume level, or 30%
-	volbar.value = stream.volume = localStorage.getItem("volumeKey") || 0.3;
-	// Volume bar listeners
-	$("#volume")
-		.change(function () {
-			stream.volume = this.value;
-			localStorage.setItem("volumeKey", this.value);
-		})
-		.on("input", function () {
-			stream.volume = this.value;
-			localStorage.setItem("volumeKey", this.value);
-		});
+   iOSUA || safariUA && alert('You appear to be using an iOS device or a Safari browser. Cadence stream playback may not be compatible with your platform.')
 
-	// Search keyup
-	$("#searchInput").keyup(function (event) {
-		if (event.keyCode == 13) {
-			postSearch();
-		}
-	});
-});
+   let play = (stream) => {
+      stream.src = streamSrcURL
+      stream.load()
+      stream.play()
+      document.getElementById(PLAY_BUTTON).innerHTML = '⏸'
+   }
+
+   let pause = (stream) => {
+      stream.src = ''
+      stream.load()
+      stream.pause()
+      document.getElementById(PLAY_BUTTON).innerHTML = '⏵'
+   }
+
+   // Loads and unloads audio stream source
+   document
+      .getElementById(PLAY_BUTTON)
+      .addEventListener('click', () => stream.paused ? play() : pause())
+
+   // Load cached volume level, or 30%
+   volbar.value = stream.volume = localStorage.getItem(VOLUME_KEY) || 0.3
+
+   // Volume bar listeners
+   $('#volume')
+      .change(() => {
+         stream.volume = this.value
+         localStorage.setItem(VOLUME_KEY, this.value)
+      })
+      .on('input', () => {
+         stream.volume = this.value
+         localStorage.setItem(VOLUME_KEY, this.value)
+      })
+
+   // Search keyup
+   $('#searchInput').keyup((event) => event.keyCode == 13 && postSearch())
+})
